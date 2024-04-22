6565ddd907b9dd05a1e6141ced4f386a9a8bbfab
MritulAich <mridulaich332@gmail.com>
2024-04-22 10:16:20 +0600

    used env variables

--- firebase.config.js
+++ firebase.config.js
@@ -4,13 +4,14 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBLMeRH17rxSPKhfn9amFpiBbZxDIMCP0o",
-  authDomain: "assignment-9-4e691.firebaseapp.com",
-  projectId: "assignment-9-4e691",
-  storageBucket: "assignment-9-4e691.appspot.com",
-  messagingSenderId: "146780593139",
-  appId: "1:146780593139:web:510d2620429368c26fd086"
+  apiKey: import.meta.env.VITE_APIKEY ,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN ,
+  projectId: import.meta.env.VITE_PROJECTID ,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET ,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID ,
+  appId: import.meta.env.VITE_APPID 
 };
 
 // Initialize Firebase
